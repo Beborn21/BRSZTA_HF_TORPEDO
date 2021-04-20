85405c2a665f0e138746f3b075c62f22225eea78
Beborn21 <81379571+Beborn21@users.noreply.github.com>
2021-04-20 20:59:43 +0200

    A küldő és fogadó tömbök már 3 elemű integert várnak és adnak

--- communicationPack/server.java
+++ communicationPack/server.java
@@ -37,16 +37,17 @@ public class server extends Network {
         outputStreamWriter.flush();
     }
 
-    public int[] ReceiveData() throws IOException {
+     public int[] ReceiveData() throws IOException {
         int x=0;
         inputStreamReader=new InputStreamReader(inputStream);
         stringBuffer=new StringBuffer();
 
-        int []data=new int[2];
-        int temp1,temp2;
+        int []data=new int[3];
+        int temp1,temp2,temp3;
         int data1;
         int data2;
-        String part1,part2;
+        int data3;
+        String part1,part2,part3;
 
         while(true)
         {
@@ -58,20 +59,25 @@ public class server extends Network {
 
         temp1=request.indexOf(",");
         part1=request.substring(1,temp1);
-        temp2=request.indexOf("]");
+        temp2=request.indexOf(",",temp1+1);
         part2=request.substring(temp1+2,temp2);
+
+        temp3=request.indexOf("]");
+        part3=request.substring(temp2+2,temp3);
 
         data1=Integer.parseInt(part1);
         data2=Integer.parseInt(part2);
+        data3=Integer.parseInt(part3);
         data[0]=data1;
         data[1]=data2;
+        data[2]=data3;
 
         return data;
     }
 
-   public boolean startGame() throws IOException {
-        int[] newGame={-1,-1};
-        int [] response=new int[2];
+    public boolean startGame() throws IOException {
+        int[] newGame={-1,-1,-1};
+        int [] response=new int[3];
         boolean isStart=false;
 
         SendData(newGame);
